ab3f6ac6561f044377cd0cdfa0c1d155cacc0167
matt-codeslo <matt@codeslo.com>
2017-05-09 23:39:05 -0700

    click example adds

--- clickExample/client.js
+++ clickExample/client.js
@@ -2,6 +2,9 @@ var contentDiv = document.getElementById('content-div');
 var btn = document.getElementById('btn');
 btn.addEventListener('click',changeDiv);
 
+
+
 function changeDiv(){
     contentDiv.innerHTML = "<h2>This is STILL a div</h2><p>...but it has new content now.</p>";
 }
+
