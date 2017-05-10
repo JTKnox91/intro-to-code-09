d255a3aaa4ca1dbd0d60e8098d9caf414af201bb
matt-codeslo <matt@codeslo.com>
2017-05-09 23:18:21 -0700

    added clickExample

---
+++ clickExample/client.js
@@ -0,0 +1,7 @@
+var contentDiv = document.getElementById('content-div');
+var btn = document.getElementById('btn');
+btn.addEventListener('click',changeDiv);
+
+function changeDiv(){
+    contentDiv.innerHTML = "<h2>This is STILL a div</h2><p>...but it has new content now.</p>";
+}
